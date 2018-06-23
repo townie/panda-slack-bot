c2344796b8e6911063e7071bb6ce76ba602bb5fe
K W <keith.e.webber@gmail.com>
2018-06-23 16:31:48 -0400

    init commit

---
+++ panda_house/test.py
@@ -0,0 +1,9 @@
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+
+@respond_to('hi', re.IGNORECASE)
+def hi(message):
+    message.reply('I can understand hi or HI!')
+    # react with thumb up emoji
+    message.react('+1')
---
+++ slackbot_settings.py
@@ -0,0 +1,9 @@
+import secrets
+
+API_TOKEN = secrets.API_TOKEN
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+ERRORS_TO = 'bot-dev'
+
+PLUGINS = [
+    'panda_house.test',
+]
