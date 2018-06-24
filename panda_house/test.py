f0474391a5c2d409eca61f7956326fc5b4a56bd0
K W <keith.e.webber@gmail.com>
2018-06-23 22:35:41 -0400

    clean up gitignore

--- panda_house/test.py
+++ panda_house/test.py
@@ -7,3 +7,9 @@ def hi(message):
     message.reply('I can understand hi or HI!')
     # react with thumb up emoji
     message.react('+1')
+
+
+@respond_to('status', re.IGNORECASE)
+def status(message):
+	message.reply('WORK IN PROGRESS')
+	message.react('construction_worker')
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,11 @@
-import secrets
+try:
+	import secrets
 
-API_TOKEN = secrets.API_TOKEN
+	API_TOKEN = secrets.API_TOKEN
+except:
+	print('ERROR: No Secrets were found')
+
+
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 ERRORS_TO = 'bot-dev'
 
