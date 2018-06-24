901030feae8536184f42f0bddaf334bbfadf13a4
K W <keith.e.webber@gmail.com>
2018-06-23 23:53:25 -0400

    improve the secrets setting

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,26 +1,41 @@
+import sys
+import os
+
+
 def load_secret(key, default=None):
+    """
+    Set module level constant dynamically from secrets.py, env or default
+    """
     key = key.upper()
+    module = sys.modules[__name__]
+    key_set = False
+
     try:
         import secrets
-        __dict__[key] = secrets.__dict__(key)
+        setattr(module, key, secrets.__dict__[key])
+        key_set = True
+
         print('INFO: Loaded {} from secrets'.format(key))
         return True
     except:
         try:
-            import os
             __dict__[key] = os.environ[key]
+            setattr(module, key, secrets.__dict__[key])
+
             print('INFO: Loaded {} from ENV'.format(key))
-            return True
+            key_set = True
 
         except:
             print('ERROR: {} not found, application may not work'.format(key))
-    return False
-        
+
+    if not key_set and deault is not None:
+        setattr(module, key, deault)
+        key_set = True
+
+    return key_set
 
 
 load_secret('API_TOKEN')
-load_secret('API_TOKEN')
-
 
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
