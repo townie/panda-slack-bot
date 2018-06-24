d57decc16eccf387923d4e7ae4a042fc4ea13f85
K W <keith.e.webber@gmail.com>
2018-06-23 22:54:31 -0400

    cleanup:

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,9 +1,26 @@
-try:
-	import secrets
+def load_secret(key):
+    key = key.upper()
+    try:
+        import secrets
+        __dict__(key) = secrets.__dict__(key)
+        print('INFO: Loaded {} from secrets'.format(key))
+        return True
+    except:
+        try:
+            import os
+            __dict__(key) = os.environ[key]
+            print('INFO: Loaded {} from ENV'.format(key))
+            return True
 
-	API_TOKEN = secrets.API_TOKEN
-except:
-	print('ERROR: No Secrets were found')
+        except:
+            print('ERROR: {} not found, application may not work'.format(key))
+    return False
+        
+
+
+load_secret('API_TOKEN')
+load_secret('API_TOKEN')
+
 
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
