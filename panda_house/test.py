73564b6d09ae3def47c00a8174880b2384d68b9d
Keith Webber <keith.e.webber@gmail.com>
2018-06-25 14:21:58 -0400

    refactor

--- panda_house/test.py
+++ panda_house/test.py
@@ -1,11 +1,11 @@
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-
-from slackbot import settings
+import re
 
 from phue import Bridge
+from simple_salesforce import Salesforce
+from slackbot import settings
+from slackbot.bot import listen_to
+from slackbot.bot import respond_to
 
-import re
 
 @respond_to('hi', re.IGNORECASE)
 def hi(message):
@@ -16,27 +16,24 @@ def hi(message):
 
 @respond_to('status', re.IGNORECASE)
 def status(message):
-	message.reply('WORK IN PROGRESS')
-	message.react('construction_worker')
-
+    message.reply('WORK IN PROGRESS')
+    message.react('construction_worker')
 
 
 @respond_to('lights', re.IGNORECASE)
 def lights(message):
-	b = Bridge('192.168.1.16')
+    b = Bridge('192.168.1.16')
 
-	# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
-	b.connect()
-	lights = b.get_light_objects('id')
+    # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+    b.connect()
+    lights = b.get_light_objects('id')
 
-	message.reply(lights)
-
+    message.reply(lights)
 
 
 @respond_to('query (.*)', re.IGNORECASE)
 def query(message, qstring):
+    sf = Salesforce(password=settings.SFCD_PASSWORD, username=settings.SFCD_USERNAME,
+                    organizationId=settings.SFDC_ORG_ID)
 
-	sf = Salesforce(password=settings.SFCD_PASSWORD, username=settings.SFCD_USERNAME,
-					organizationId=settings.SFDC_ORG_ID)
-
-	message.reply(sf.query(qstring))
+    message.reply(sf.query(qstring))
