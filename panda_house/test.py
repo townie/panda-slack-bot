b80bde10e651a9cb93586aa30265089b3ed7d6e9
Keith Webber <keith.e.webber@gmail.com>
2018-06-25 10:59:01 -0400

    lights

--- panda_house/test.py
+++ panda_house/test.py
@@ -1,5 +1,8 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
+
+from phue import Bridge
+
 import re
 
 @respond_to('hi', re.IGNORECASE)
@@ -13,3 +16,15 @@ def hi(message):
 def status(message):
 	message.reply('WORK IN PROGRESS')
 	message.react('construction_worker')
+
+
+
+@respond_to('lights', re.IGNORECASE)
+def lights(message):
+	b = Bridge('192.168.1.16')
+
+	# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+	b.connect()
+	lights = b.get_light_objects('id')
+
+	message.reply(lights)
