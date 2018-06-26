4d51fd9dd9f90b715f762e0fd398de5a6b46ee8a
K W <keith.e.webber@gmail.com>
2018-06-26 02:35:44 -0400

    refactor to requests

--- keith/crypto.py
+++ keith/crypto.py
@@ -1,8 +1,10 @@
-from six.moves.urllib import request
+# from six.moves.urllib import request
 import json
 import threading
 import locale
 import re
+
+import requests
 
 from slackbot import settings
 from slackbot.bot import listen_to
@@ -18,22 +20,22 @@ def mining_stats(message):
         settings.NICEHASH_ADDRESS
     price = 'http://api.coindesk.com/v1/bpi/currentprice/'+settings.BITCOIN_FIAT+'.json'
 
-    reqStats = urllib.request.Request(stats)
+    reqStats = requests.get(stats)
 
     stop_event = threading.Event()
 
     # parsing currency
     locale.setlocale(locale.LC_ALL, '')
-    reqPrice = urllib.request.Request(price)
-    rPrice = urllib.request.urlopen(reqPrice).read()
+    reqPrice = requests.get(price)
+    rPrice =requests.get(reqPrice).content
     priceCurrency = float(json.loads(rPrice)['bpi'][currency]['rate_float'])
     reply += "\n\nUsing Currency: BTC/{0} = {1:,.2f}".format(
         currency, priceCurrency)
     reply += "=======================================\n"
 
     # parsing response
-    rStats = urllib.request.urlopen(reqStats).read()
-    cont = json.loads(rStats.decode('utf-8'))
+    cont =  requests.get(reqStats).json()
+    # cont = json.loads(rStats.decode('utf-8'))
     counter = 0
     balance = 0
     totalWorkers = 0
@@ -48,8 +50,8 @@ def mining_stats(message):
 
         worker = 'https://api.nicehash.com/api?method=stats.provider.workers&addr=' + \
             addr+'&algo='+str(item['algo'])
-        reqWorkers = urllib.request.Request(worker)
-        rWorker = urllib.request.urlopen(reqWorkers).read()
+        reqWorkers = request.get(worker)
+        rWorker =  request.get(reqWorkers).content
         totalWorkers += len(json.loads(rWorker)['result']['workers'])
         reply += "Workers: {0}\n".format(len(json.loads(rWorker)
                                              ['result']['workers']))
