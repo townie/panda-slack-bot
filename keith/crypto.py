3340791038b0b0a46a17f0f6207d58a4d27464ad
K W <keith.e.webber@gmail.com>
2018-06-26 02:16:08 -0400

    actually reply

--- keith/crypto.py
+++ keith/crypto.py
@@ -10,9 +10,8 @@ from slackbot.bot import respond_to
 
 @respond_to('mining stats', re.IGNORECASE)
 def mining_stats(message):
-    message.reply('I can understand hi or HI!')
 
-    repy = ''
+    reply = ''
 
     stats = 'https://api.nicehash.com/api?method=stats.provider.ex&addr='+settings.NICEHASH_ADDRESS
     price = 'http://api.coindesk.com/v1/bpi/currentprice/'+settings.BITCOIN_FIAT+'.json'
@@ -26,8 +25,8 @@ def mining_stats(message):
     reqPrice = urllib.request.Request(price)
     rPrice = urllib.request.urlopen(reqPrice).read()
     priceCurrency=float(json.loads(rPrice)['bpi'][currency]['rate_float'])
-    repy += "\n\nUsing Currency: BTC/{0} = {1:,.2f}".format(currency,priceCurrency)
-    repy += "=======================================\n"
+    reply += "\n\nUsing Currency: BTC/{0} = {1:,.2f}".format(currency,priceCurrency)
+    reply += "=======================================\n"
 
     ##parsing response
     rStats = urllib.request.urlopen(reqStats).read()
@@ -42,29 +41,31 @@ def mining_stats(message):
         counter += 1
         balance+=float(item['data'][1])
 
-        repy += "Algo: ({0}) {1}\n".format(item['algo'],item['name'])
+        reply += "Algo: ({0}) {1}\n".format(item['algo'],item['name'])
 
 
         worker = 'https://api.nicehash.com/api?method=stats.provider.workers&addr='+addr+'&algo='+str(item['algo'])
         reqWorkers = urllib.request.Request(worker)
         rWorker = urllib.request.urlopen(reqWorkers).read()
         totalWorkers += len(json.loads(rWorker)['result']['workers'])
-        repy += "Workers: {0}\n".format(len(json.loads(rWorker)['result']['workers']))
+        reply += "Workers: {0}\n".format(len(json.loads(rWorker)['result']['workers']))
         if (len(item['data'][0])>=1):
-            repy += "Accepted Speed: {0} {1}/s\n".format(item['data'][0]['a'],item['suffix'])
-            repy += "Profitability: {0} BTC/day or {1:,.2f} {2}/day\n".format(float(item['profitability'])*float(item['data'][0]['a']),float(item['profitability'])*float(item['data'][0]['a'])*priceCurrency, currency)
+            reply += "Accepted Speed: {0} {1}/s\n".format(item['data'][0]['a'],item['suffix'])
+            reply += "Profitability: {0} BTC/day or {1:,.2f} {2}/day\n".format(float(item['profitability'])*float(item['data'][0]['a']),float(item['profitability'])*float(item['data'][0]['a'])*priceCurrency, currency)
         else:
-            repy += "Accepted Speed: 0 {0}/s\n".format(item['suffix'])
-            repy += "Profitability: 0 BTC/day or 0.00 {0}/day\n".format(currency)
+            reply += "Accepted Speed: 0 {0}/s\n".format(item['suffix'])
+            reply += "Profitability: 0 BTC/day or 0.00 {0}/day\n".format(currency)
 
 
         if (len(json.loads(rWorker)['result']['workers'])>=1):
             profitability+=float(float(item['profitability'])*float(item['data'][0]['a']))
-        repy += "Balance: {0} BTC or {1:,.2f} {2}\n".format(item['data'][1],float(item['data'][1])*priceCurrency, currency)
-        repy += "---------------------------------------------------"
+        reply += "Balance: {0} BTC or {1:,.2f} {2}\n".format(item['data'][1],float(item['data'][1])*priceCurrency, currency)
+        reply += "---------------------------------------------------"
 
-    repy += "==================================================="
-    repy += "Total Algo: \n", counter
-    repy += "Total Workers: {0}\n".format(totalWorkers)
-    repy += "Total Profitability: {0} BTC/day or {1:,.2f} {2}\n".format(profitability, float(profitability)*priceCurrency, currency)
-    repy += "Total Balance: ", balance, "BTC\n"
+    reply += "==================================================="
+    reply += "Total Algo: \n", counter
+    reply += "Total Workers: {0}\n".format(totalWorkers)
+    reply += "Total Profitability: {0} BTC/day or {1:,.2f} {2}\n".format(profitability, float(profitability)*priceCurrency, currency)
+    reply += "Total Balance: ", balance, "BTC\n"
+
+    message.reply(reply)
