bd8b039abb46be4abdebb422f5c02d368e1eb04e
Keith Webber <keith.e.webber@gmail.com>
2018-06-25 21:41:03 -0400

    on/off

--- panda_house/panda.py
+++ panda_house/panda.py
@@ -30,10 +30,14 @@ def lights(message):
     b.connect()
     lights = b.get_light_objects('id')
 
-    light_status = {light.name: bool(light.on) for light in lights.values()}
+    light_status = {light.name: light.on for light in lights.values()}
 
     message.reply(codeblock(convert_to_readable(light_status, headers=["Light", "Status"])))
 
+def strbool(thing):
+    if thing:
+        return 'ON'
+    return "OFF"
 
 def codeblock(s):
     return '```\n' + s + '```'
@@ -43,11 +47,11 @@ def convert_to_readable(dictionary, headers=None):
     strout = ''
 
     if headers:
-        strout = "{:<20} {:<35}\n".format(headers[0], headers[1])
+        strout = "{:<20} | {:<35}\n".format(headers[0], headers[1])
 
     strout += '-' * 40 + '\n'
     for k, v in dictionary.items():
-        strout += "{:<20} {:<35}\n".format(k, v)
+        strout += "{:<20} | {:<35}\n".format(k, v)
 
     return strout
 
